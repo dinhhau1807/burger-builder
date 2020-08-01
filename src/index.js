35827193c000fba88649efd380fcf12ac97e9520
NDHau <nguyendinhhau1807@gmail.com>
2020-08-01 17:17:24 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import authReducer from './store/reducers/auth';
 const composeEnhancers =
   process.env.NODE_ENV === 'development'
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-    : null;
+    : compose;
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
